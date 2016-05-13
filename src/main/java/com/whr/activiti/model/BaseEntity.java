e922231dbd41fd28603126725d4a81bb091d16c3
Lyon <lyon_z@msn.com>
2016-05-13 14:52:57 +0800

    init

--- src/main/java/com/whr/activiti/model/BaseEntity.java
+++ src/main/java/com/whr/activiti/model/BaseEntity.java
@@ -10,12 +10,13 @@ public class BaseEntity implements Serializable{
 	
 	private Integer id;
 	
+
 	public Integer getId() {
 		return id;
 	}
 	
 	public void setId(Integer id) {
 		this.id = id;
-	}
+	} 
 
 }
